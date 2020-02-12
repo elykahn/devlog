0e119f05777d3643ba15f23d1389dc5f0536658b
Joshua Ely Kahn <ely.kahn@gmail.com>
2020-02-12 04:23:48 -0600

    Update index.js

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Card = styled.div`
-  background: #347ea6;
+  background: #d1d6e5;
   border-radius: 10px;
   box-shadow: -11px 11px 46px #2235671,
 11px -11px 46px #45a6db;
